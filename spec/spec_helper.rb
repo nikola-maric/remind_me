111e68061dbde8ad208157b0e84c146fe9795b9f
nikola <nikola@deversity.net>
2023-04-29 12:35:25 +0200

    Start coverage testing only if JRuby is not being used

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 require 'simplecov'
 
-if RSpec.configuration.files_to_run.size > 1
+# Setting up simplecov to work properly with JRuby is a hassle, so we can ignore it - other builds will still use it though
+if RSpec.configuration.files_to_run.size > 1 && RUBY_ENGINE != 'jruby'
   SimpleCov.minimum_coverage_by_file 80
   SimpleCov.minimum_coverage 90
   SimpleCov.start
